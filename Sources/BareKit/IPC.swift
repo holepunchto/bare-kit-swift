2655a8af84dfa23057bd1bc4f552b8a6323c263b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-05-08 12:35:42 +0200

    Update for latest Bare Kit

--- Sources/BareKit/IPC.swift
+++ Sources/BareKit/IPC.swift
@@ -8,46 +8,12 @@ public struct IPC: AsyncSequence {
     self.ipc = BareIPC(worklet: worklet.worklet)!
   }
 
-  public func read() async -> Data {
-    if let data = ipc.read() {
-      return data
-    }
-
-    assert(ipc.readable == nil)
-
-    return await withCheckedContinuation { continuation in
-      ipc.readable = { ipc in
-        if let data = ipc.read() {
-          ipc.readable = nil
-
-          continuation.resume(returning: data)
-        }
-      }
-    }
+  public func read() async throws -> Data? {
+    return try await ipc.read()
   }
 
-  public func read(encoding: String.Encoding) async -> String {
-    return String(data: await read(), encoding: encoding)!
-  }
-
-  public func write(data: Data) async {
-    if ipc.write(data) {
-      return
-    }
-
-    return await withCheckedContinuation { continuation in
-      ipc.writable = { ipc in
-        if ipc.write(data) {
-          ipc.writable = nil
-
-          continuation.resume()
-        }
-      }
-    }
-  }
-
-  public func write(data: String, encoding: String.Encoding) async {
-    return await write(data: data.data(using: encoding)!)
+  public func write(data: Data) async throws {
+    return try await ipc.write(data)
   }
 
   public func close() {
@@ -59,8 +25,8 @@ public struct IPC: AsyncSequence {
   public struct AsyncIterator: AsyncIteratorProtocol {
     let ipc: IPC
 
-    public func next() async -> Data? {
-      return await ipc.read()
+    public func next() async throws -> Data? {
+      return try await ipc.read()
     }
   }
 
